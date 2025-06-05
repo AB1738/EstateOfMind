b02fc71620c6a33ddac97daf9da809b1b7271416
AB1738 <saintsring2017@yahoo.com>
2025-06-05 18:14:44 -0400

    updated property card ui

--- frontend/src/components/customComponents/PropertyGrid.tsx
+++ frontend/src/components/customComponents/PropertyGrid.tsx
@@ -3,15 +3,12 @@
 import { Properties } from "@/types/PropertyTypes";
 import Image from "next/image";
 import Link from "next/link";
+import { Bed, Bath } from "lucide-react";
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface GridPropType {
   properties: Properties;
@@ -26,24 +23,54 @@ const PropertyGrid = ({ properties }: GridPropType) => {
           key={property.id}
           className="hover:scale-101  "
         >
-          <Card className="h-full">
-            <CardHeader>
-              <CardTitle>{property.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Image
-                height={1000}
-                width={1000}
-                alt="Property Image"
-                src={property.imageUrl}
-                className="h-50"
-              />
-              <CardDescription>{property.description}</CardDescription>
-            </CardContent>
-            <CardFooter>
-              <p>{property.price}</p>
-            </CardFooter>
-          </Card>
+          <div className="flex flex-col bg-white items-center rounded gap-1.5 pb-3">
+            <Image
+              height={1000}
+              width={1000}
+              alt="Property Image"
+              src={property.imageUrl}
+              className="h-70 w-full rounded-t"
+            />
+            <h2 className="font-semibold">{property.title}</h2>
+            <div className="flex gap-1.5">
+              <p>{property.address},</p>
+              <p>{property.city},</p>
+              <p>{property.state}</p>
+            </div>
+            <p className="flex gap-1 items-center">
+              {new Intl.NumberFormat().format(property.sqFt)} SqFt
+            </p>
+            <div className="flex gap-1.5">
+              <div className="flex gap-1 items-center">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <p className="flex gap-1 items-center">
+                      {property.bedrooms} <Bed size={20} />
+                    </p>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p> {property.bedrooms} Bed</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+              <div>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <p className="flex gap-1 items-center">
+                      {property.bathrooms} <Bath size={20} />
+                    </p>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p> {property.bedrooms} Bath</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            </div>
+
+            <p className="font-bold">
+              ${new Intl.NumberFormat().format(property.price)}
+            </p>
+          </div>
         </Link>
       ))}
     </div>
